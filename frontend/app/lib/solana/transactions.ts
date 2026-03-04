9faf8d06710257e27a45ff984b1e82e8747b5f7f
makimakiver2 <k23168817@kcl.ac.uk>
2026-03-04 19:24:50 +0000

    README updated

--- frontend/app/lib/solana/transactions.ts
+++ frontend/app/lib/solana/transactions.ts
@@ -3,30 +3,28 @@ import {
   PublicKey,
   SystemProgram,
   Transaction as SolanaTransaction,
-  TransactionInstruction,
+  // TransactionInstruction,
 } from "@solana/web3.js";
 
-const MEMO_PROGRAM_ID = new PublicKey(
-  "MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr",
-);
+const MEMO_PROGRAM_ID = "";
 
-export async function buildUnsignedMemoTx(
-  connection: Connection,
-  from: PublicKey,
-  memoText: string,
-) {
-  const tx = new SolanaTransaction().add(
-    new TransactionInstruction({
-      keys: [{ pubkey: from, isSigner: true, isWritable: true }],
-      programId: MEMO_PROGRAM_ID,
-      data: Buffer.from(memoText, "utf-8"),
-    }),
-  );
-  const { blockhash } = await connection.getLatestBlockhash("confirmed");
-  tx.recentBlockhash = blockhash;
-  tx.feePayer = from;
-  return { tx, messageBytes: tx.serializeMessage() };
-}
+// export async function buildUnsignedMemoTx(
+//   connection: Connection,
+//   from: PublicKey,
+//   memoText: string,
+// ) {
+//   const tx = new SolanaTransaction().add(
+//     new TransactionInstruction({
+//       keys: [{ pubkey: from, isSigner: true, isWritable: true }],
+//       programId: MEMO_PROGRAM_ID,
+//       data: Buffer.from(memoText, "utf-8"),
+//     }),
+//   );
+//   const { blockhash } = await connection.getLatestBlockhash("confirmed");
+//   tx.recentBlockhash = blockhash;
+//   tx.feePayer = from;
+//   return { tx, messageBytes: tx.serializeMessage() };
+// }
 
 export async function buildUnsignedSOLTransfer(
   connection: Connection,
